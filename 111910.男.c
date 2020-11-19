d53b59dd2451ce542e9b668171490e77059569f6
BenDNN <74294696+BenDNN@users.noreply.github.com>
2020-11-19 19:49:26 +0800

    Create 111910.男.c

---
+++ 111910.男.c
@@ -0,0 +1,20 @@
+#include<stdio.h>
+void main(){
+int n;
+
+scanf("%d",&n);
+if(n==1){
+printf("男 !\n");
+}
+
+else if(n==0){
+printf("女 !\n");
+}
+
+
+else{
+printf("输出错误!\n");
+}
+}
+
+//我的错误：双等号才是表判断
