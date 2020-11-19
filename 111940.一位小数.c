37664bacd066046004dc823d794f460444407bd5
BenDNN <74294696+BenDNN@users.noreply.github.com>
2020-11-19 20:04:20 +0800

    Create 111940.一位小数.c

---
+++ 111940.一位小数.c
@@ -0,0 +1,10 @@
+#include<stdio.h>
+void main(){
+	double eat[5]={1.1,2.2,3.3,4.4,5.5};
+	printf("%.1lf\n",eat[0]);
+	printf("%.1lf\n",eat[1]);
+	printf("%.1lf\n",eat[2]);
+	printf("%.1lf\n",eat[3]);
+	printf("%.1lf\n",eat[4]);
+
+}
