46a34aaaf3544ecd84cb74d10c2227b8bf6f919f
BenDNN <74294696+BenDNN@users.noreply.github.com>
2020-11-25 17:33:37 +0800

    Update 010-模拟延时程序.离谱.c

--- 010-模拟延时程序.离谱.c
+++ 010-模拟延时程序.离谱.c
@@ -1,9 +1,26 @@
+内容1：
 #include<stdio.h>
 #include<windows.h                                                                                                  >
 void main(){
 
-	printf("天气冷了,记得多喝热水好吗!\n");
+	printf("多喝热水!\n");
 	Sleep(3000);
-		printf("麻了个喵的!你家热水是观音那求来的？能包治百病吗?!\n");
+	printf("分手吧!.......\n");
+}
+
+内容2：
+#include<stdio.h>
+#include<windows.h>
+void main(){
+
+	while(1){
+	printf("男:去吧!\n");
+	
+	Sleep(3000);	
+	printf("女:真不错!\n");
+
+	Sleep(3000);
+}	
+
 
 }
