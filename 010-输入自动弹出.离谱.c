a848d7000cf6cc7ab2045bf315ba0cbb8c31f08b
BenDNN <74294696+BenDNN@users.noreply.github.com>
2020-11-25 16:47:36 +0800

    Create 010-输入自动弹出.离谱.c

---
+++ 010-输入自动弹出.离谱.c
@@ -0,0 +1,9 @@
+#include<stdio.h>
+#include<windows.h                                                                                                  >
+void main(){
+
+	printf("天气冷了,记得多喝热水好吗!\n");
+	Sleep(3000);
+		printf("麻了个喵的!你家热水是观音那求来的？能包治百病吗?!\n");
+
+}
