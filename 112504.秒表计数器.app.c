7033093f9f9be29d3671c6a0e3e50f76fe2224fa
BenDNN <74294696+BenDNN@users.noreply.github.com>
2020-11-26 19:43:18 +0800

    Update 112504.秒表计数器.app.c

--- 112504.秒表计数器.app.c
+++ 112504.秒表计数器.app.c
@@ -1,14 +1,12 @@
 #include<stdio.h>
 #include<windows.h>
 void main(){
-	 
-		int n=1;
-		printf("欢迎使用虫小军版秒表计数器:\n");
-	    while(1){
+	int n=1;
+	printf("欢迎使用虫小军牌计时器\n");
 
-		Sleep(3000);
-		printf("%d秒过去了喂!还不来我走了!...\n",n);
-
-		n++;
-}
-}
+	while(1){
+	Sleep(3000);
+	printf("%d秒过去了喂!小廖快点啊!嗯....懒虫!太阳都晒屁股了你!\n",n);
+	n++;
+	}
+	}
