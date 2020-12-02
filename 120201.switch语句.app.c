db592431f660d470c7d3dcf78ad08d58459642d0
BenDNN <74294696+BenDNN@users.noreply.github.com>
2020-12-02 09:03:10 +0800

    Create 120201.switch语句.app.c

---
+++ 120201.switch语句.app.c
@@ -0,0 +1,28 @@
+#include<stdio.h>
+void main(){
+int n=6;
+switch(n){
+	case 1:printf("清华大学\n");break;
+	case 2:printf("北京大学\n");break;
+	case 3:printf("中国科学技术大学\n");break;
+	case 4:printf("浙江大学\n");break;
+	case 5:printf("复旦大学\n");break;
+	case 6:printf("南京大学\n");break;
+	case 7:printf("上海交通大学\n");break;
+	case 8:printf("中山大学\n");break;
+	case 9:printf("北京师范大学\n");break;
+	case 10:printf("华中科技大学\n");break;
+	case 11:printf("南方科技大学\n");break;
+	case 12:printf("南开大学\n");break;
+	case 13:printf("武汉大学\n");break;
+	case 14:printf("中南大学\n");break;
+	case 15:printf("哈尔滨工业大学\n");break;
+	case 16:printf("湖南大学\n");break;
+	case 17:printf("同济大学\n");break;
+	case 18:printf("北京航空航天大学\n");break;
+	case 19:printf("华东师范大学\n");break;
+	case 20:printf("中国人民大学\n");break;
+
+	  default:printf("请输入一个1-20之间的数字!\n");
+}
+}
