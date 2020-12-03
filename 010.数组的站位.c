0f64bda2e44ed5fd9a57693f13dba1d877f4a03d
BenDNN <74294696+BenDNN@users.noreply.github.com>
2020-12-03 20:44:49 +0800

    Create 010.数组的站位.c

---
+++ 010.数组的站位.c
@@ -0,0 +1,15 @@
+#include<stdio.h>
+void main(){
+	int i,n;
+	int array[]={2020,2021,2022,2023,2024};
+	int array2d[2][3]={
+		{1,2,3},
+		{4,5,6}
+	};
+	for(i=0;i<2;i++){
+		for(n=0;n<3;n++){
+	printf("%d ",array2d[i][n]);
+}
+		printf("\n");
+}
+}
