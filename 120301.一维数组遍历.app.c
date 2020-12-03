26a38a772747ad707669c884c84b331566bc9300
BenDNN <74294696+BenDNN@users.noreply.github.com>
2020-12-03 20:58:59 +0800

    Create 120301.一维数组遍历.app.c

---
+++ 120301.一维数组遍历.app.c
@@ -0,0 +1,13 @@
+#include<stdio.h>
+void main(){
+	int i,n;
+	int array[]={2021,2022,2023,2024,2025,2026};
+	int array2d[2][3]={
+		{1,2,3},
+		{4,5,6}
+	};
+	
+	for(i=0;i<6;i++){
+	printf("%d \n",array[i]);
+}
+}
