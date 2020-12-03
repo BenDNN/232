885c9773fe352327e02fa02293bcab6e59be03be
BenDNN <74294696+BenDNN@users.noreply.github.com>
2020-12-03 20:02:19 +0800

    Update 112603.升序排序.app.c

--- 112603.升序排序.app.c
+++ 112603.升序排序.app.c
@@ -1,33 +1,14 @@
 #include<stdio.h>
-void main(){
+int main(){
 	
-	int a,b,c;
-	printf("请输入三个数字\n");
+	int a,b,c,i;
+	printf("请输入三个数字,我们将为你自动升序排序!\n");
 	scanf("%d%d%d",&a,&b,&c);
 
-	if(a<b&&a<c){
-		if(b<c){
-		printf("%d\n%d\n%d\n",a,b,c);
-	}
-		else{
-		printf("%d\n%d\n%d\n",a,c,b);
-		}
-}
-	else if(b<a&&b<c){
-		if(a<c){
-		printf("%d\n%d\n%d\n",b,a,c);
-		}
-		else{
-		printf("%d\n%d\n%d\n",b,c,a);
-		}
-	}
-		else if(c<a&&c<b){
-		if(a<b){
-		printf("%d\n%d\n%d\n",c,a,b);
-		}
-		else{
-		printf("%d\n%d\n%d\n",c,b,a);
-		}
-	}
-	
+	if(a>b){i=a;a=b;b=i;}
+	if(a>c){i=a;a=c;c=i;}
+	if(b>c){i=b;b=c;c=i;}
+
+	printf("%d\n%d\n%d\n",a,b,c);
+	return 0;
 }
