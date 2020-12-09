b03b4f0dd4dfa77b7c2b687ace9541fc29e246f8
BenDNN <74294696+BenDNN@users.noreply.github.com>
2020-12-09 16:52:53 +0800

    Create 120904.串比对.app.c

---
+++ 120904.串比对.app.c
@@ -0,0 +1,13 @@
+#include<stdio.h>
+#include<string.h>
+void main(){
+	char s1[]="广西科技师范学院!";
+	char s2[]="广东科技师范学院!";
+//string com巴拉巴拉=strcmp;strcmp
+	if(strcmp(s1,s2)==0){
+	printf("小廖脑壳儿no problem!\n");
+	}
+	else{
+	printf("小廖脑壳儿have problem!\n");
+	}
+}
