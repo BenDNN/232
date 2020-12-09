993146b38619601da8f4f4cdaef1dcd22676a706
BenDNN <74294696+BenDNN@users.noreply.github.com>
2020-12-09 17:16:20 +0800

    Create 120905.串长度.app.c

---
+++ 120905.串长度.app.c
@@ -0,0 +1,12 @@
+#include<stdio.h>
+#include<string.h>
+	void main(){
+	char s1[]="world";
+	//字母个数+1（九尾狐）
+	char s2[]="心悦君兮君不知";
+	//2*中文个数+1
+
+		printf("%d",sizeof(s1));
+		printf("\n");
+		printf("%d\n",sizeof(s2));
+}
